9ad3c924b73dd5f29e6fa487f76e74b4a89ce987
Nicolas Espinosa <nico.2011.pr@gmail.com>
2026-02-27 20:15:14 -0300

    Agregado comando para despertar servicio de Java

--- deploy-test.sh
+++ deploy-test.sh
@@ -1,6 +1,12 @@
-'#!/bin/bash' 
-'echo "------------------------------------------"' 
-'echo "LOG: Iniciando proceso de deploy simulado"' 
-'echo "LOG: Actualizando repositorio de Java..."' 
-'echo "? DEPLOY FINALIZADO CON EXITO"' 
-'echo "------------------------------------------"' 
+#!/bin/bash
+echo "--- 🚀 WEBHOOK RECIBIDO ---"
+echo "LOG: El servicio de Java está suspendido/detenido."
+echo "LOG: Enviando orden de encendido a Render..."
+
+# ESTA LÍNEA ES LA QUE HACE LA MAGIA
+# Reemplaza lo que está entre comillas con la URL que copiaste en el paso anterior
+curl -X POST "https://api.render.com/deploy/srv-d3om0uuuk2gs73dopk8g?key=Ns-sT_vwHk0"
+
+echo ""
+echo "✅ SEÑAL ENVIADA EXITOSAMENTE"
+echo "--- Render está levantando el servicio de Java ahora mismo ---"
